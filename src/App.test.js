f071a7d5fa5f8cb584d8f2b9c7d467ec9a69543e
VictorLeung91 <victorleung91@gmail.com>
2026-04-08 08:43:53 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the user name on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/victor leung\./i);
+  expect(nameElement).toBeInTheDocument();
 });
